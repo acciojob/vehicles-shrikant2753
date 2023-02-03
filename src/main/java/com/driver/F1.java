1ea987bc6dc3e0bba68e8053c7e16a2e2d8d69b6
shrikant2753 <shrikantrp.00@gmail.com>
2023-02-03 17:26:30 +0530

    my solution

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -9,7 +9,7 @@ public class F1 extends Car {
 
     public void accelerate(int rate){
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
-        newSpeed += getCurrentSpeed();
+        newSpeed = rate + getCurrentSpeed();
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
